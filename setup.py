feaa8e2f85929edec0c1858d878a6c53a7499755
Frederik Elwert <frederik.elwert@rub.de>
2015-04-11 14:47:18 +0200

    Bump version number to 0.2.4.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='TCFlib',
-      version='0.2.3',
+      version='0.2.4',
       description='Python TCF library',
       author='Frederik Elwert',
       author_email='frederik.elwert@web.de',
