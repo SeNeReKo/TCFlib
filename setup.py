a4152a0c2ec1b2b2dd567b1b7092b43cae9b6e46
Frederik Elwert <frederik.elwert@rub.de>
2023-05-25 09:26:31 +0200

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ import setuptools
 from distutils.core import setup
 
 setup(name='TCFlib',
-      version='0.2.6',
+      version='0.3.0',
       description='Python TCF library',
       author='Frederik Elwert',
       author_email='frederik.elwert@web.de',
       url='https://github.com/SeNeReKo/TCFlib',
       packages=['tcflib', 'tcflib.tagsets'],
       package_data={'tcflib.tagsets': ['data/dc-1345.dcif']},
-     )
+      )
